05419fd3e0e41c1101f6e4c95483b3628ba15254
Gustav Berloty <gustav.berloty@epita.fr>
2024-10-27 16:22:40 +0100

    second version : greedy function is now used

--- src/lib.rs
+++ src/lib.rs
@@ -48,18 +48,8 @@ use unicode_normalization::UnicodeNormalization;
 /// ```
 pub fn core_text(mut s : String) -> String {
 
-    /* We remove punctuation and others signs. ------------------------
-    Warning: some softwares may use '|', '(' and ')' to manage nomina sacras */
-    s = s.replace(&['¶', '⋄', '?', '!', '–', ':', 
-    ';' /* greek question mark u+037e */, 
-    ';' /* semicolon u+003b */,
-    ',', '.', '·', '“', '”', '‘', '’', '᾽',
-    'ʼ', '*', '[', ']', '…', '⟦', '⟧', '|', '(', ')'], "");
-
-    // We remove any spaces. --------------------------------------------
-    s = s.chars().filter(|c| !c.is_whitespace()).collect();
-
-    // We remove diacritics signs. --------------------------------------
+    // We remove diacritics signs. Doing it now avoids the greedy 
+    // format to remove some greek letters (the accentued ones).
     const LEN: usize = '\u{036f}' as usize - '\u{0300}' as usize;
     let mut arr = ['\0'; LEN];
     for (item, ch) in std::iter::zip(&mut arr, '\u{0300}'..='\u{036f}') {
@@ -67,12 +57,8 @@ pub fn core_text(mut s : String) -> String {
     }
     s = s.nfd().to_string().replace(arr, "");
 
-    // We remove any digit.
-    s = s.chars().filter(|c| 
-               !(*c >= '\u{0030}' && *c <= '\u{0039}') // digit
-            ).collect();
-
-    replace(s)
+    s = replace(s);
+    greedy_format(s.as_str())
 }
 
 fn replace (mut s : String) -> String {
@@ -87,23 +73,15 @@ fn replace (mut s : String) -> String {
     s.replace(&['σ', 'ς'], "ϲ")
 }
 
-/* I didn't delete this function in order to show that 
-   a greedy formating that says "keep only the greek text
-   and remove anything that is not a greek character" is
-   actually not that obvious.
-*/
-#[allow(dead_code)]
 fn greedy_format(s : &str) -> String {
 
     #[allow(non_snake_case)]
-    let mut S : String = String::from(s);
-    S = replace(S);
+    let S = String::from(s);
     
     // We remove any character that is not a greek character.
     S.chars().filter(|c| 
-                           *c >= '\u{03B1}' && *c <= '\u{03C9}'     // lowercases
-                        || *c >= '\u{0391}' && *c <= '\u{03A9}'     // upercases
-                        || *c >= '\u{10140}' && *c <= '\u{1018E}'   // digits
+                           *c >= '\u{03B1}' && *c <= '\u{03C9}' // lowercases
+                        || *c == '\u{03F2}'                     // the lunar sigma
                     )
              .collect::<String>()
 }
@@ -111,12 +89,6 @@ fn greedy_format(s : &str) -> String {
 #[cfg(test)]
 mod tests {
     use super::*;
-
-    #[test]
-    #[ignore]
-    fn test_greedy_format() {
-        assert_eq!(greedy_format("Hi Πέτρος!"), String::from("πετροϲ"));
-    }
     
     #[test]
     fn test_core_text() {
