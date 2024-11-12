a5d64a422bffd2fc630f224806203c0f30647a51
Gustav Berloty <gustav.berloty@epita.fr>
2024-11-12 14:50:24 +0100

    enhanced lib.rs for testing

--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,13 @@
-//! Tools to help studying the greek new testament.
+/! Tools to help studying the greek new testament.
 //! The crate is provided AS-IS.
 //! # Examples
-//! 
+//!
 //! ```
 //! use gnt_tools::core_text;
 //!
 //! let s = "16 Εἶπεν δὲ παραβολὴν πρὸς αὐτοὺς λέγων·
-//!          ἀνθρώπου τινὸς πλουσίου εὐφόρησεν ἡ χώρα. 17 
-//!          καὶ διελογίζετο ἐν ἑαυτῷ λέγων· τί ποιήσω, ὅτι 
+//!          ἀνθρώπου τινὸς πλουσίου εὐφόρησεν ἡ χώρα. 17
+//!          καὶ διελογίζετο ἐν ἑαυτῷ λέγων· τί ποιήσω, ὅτι
 //!          οὐκ ἔχω ποῦ συνάξω τοὺς καρπούς μου; ";
 //!
 //! let s2 = "ειπενδεπαραβοληνπροϲαυτουϲλεγωνανθρωπουτ\
@@ -24,17 +24,17 @@ use unicode_normalization::UnicodeNormalization;
 ///
 /// In concrete terms, it remove diacritic signs, remove any character that is not in the greek alphabet, puts all greek letters in lowercase, and change all sigmas to lunar sigma.
 ///
-/// So this function : 
+/// So this function :
 /// - does not replace nomina sacras (e.g., κϲ) by their non-abreviated form (resp. κυριοϲ), nor words (e.g., κύριος) by their nomina sacras form (when a nomina sacra form exists) (resp. κϲ).
 /// - is made to delete any character used to encode nomina sacras (e.g., '|', or '(' and ')'), hence |κς| will give κϲ.
 /// - does delete all 'ˉ' characters (so παραβολὴˉ becomes παραβολη, not παραβολην)
-/// # Example : 
+/// # Example :
 /// ```
 /// use gnt_tools::core_text;
 ///
 /// let s = "16 Εἶπεν δὲ παραβολὴν πρὸς αὐτοὺς λέγων·
-///          ἀνθρώπου τινὸς πλουσίου εὐφόρησεν ἡ χώρα. 17 
-///          καὶ διελογίζετο ἐν ἑαυτῷ λέγων· τί ποιήσω, ὅτι 
+///          ἀνθρώπου τινὸς πλουσίου εὐφόρησεν ἡ χώρα. 17
+///          καὶ διελογίζετο ἐν ἑαυτῷ λέγων· τί ποιήσω, ὅτι
 ///          οὐκ ἔχω ποῦ συνάξω τοὺς καρπούς μου; ";
 ///
 /// let s2 = "ειπενδεπαραβοληνπροϲαυτουϲλεγωνανθρωπουτ\
@@ -43,9 +43,8 @@ use unicode_normalization::UnicodeNormalization;
 ///
 /// assert_eq!(core_text(String::from(s)), String::from(s2));
 /// ```
-pub fn core_text(mut s : String) -> String {
-
-    // We remove diacritics signs. Doing it now avoids the greedy_format 
+pub fn core_text(mut s: String) -> String {
+    // We remove diacritics signs. Doing it now avoids the greedy_format
     // function to remove some greek letters (the accentued ones).
     const LEN: usize = '\u{036f}' as usize - '\u{0300}' as usize;
     let mut arr = ['\0'; LEN];
@@ -58,39 +57,39 @@ pub fn core_text(mut s : String) -> String {
     greedy_format(s.as_str())
 }
 
-fn replace (mut s : String) -> String {
-    
+fn replace(mut s: String) -> String {
     // We remplace any "invisible nu" by a "true one". ------------------
     // EDIT : not anymore (because it's not easy to manage), but I hope later.
     // s = s.replace("ˉ", "ν");
-    
+
     // We change any uppercase letter to lowercase. ---------------------
     s = s.to_lowercase();
-    
+
     // We replace every sigmas to the lunar sigma. ----------------------
     s.replace(&['σ', 'ς'], "ϲ")
 }
 
-fn greedy_format(s : &str) -> String {
-
+fn greedy_format(s: &str) -> String {
     #[allow(non_snake_case)]
     let S = String::from(s);
-    
+
     // We remove any character that is not a greek character.
-    S.chars().filter(|c| 
-                           *c >= '\u{03B1}' && *c <= '\u{03C9}' // lowercases
-                        || *c == '\u{03F2}'                     // the lunar sigma
-                    )
-             .collect::<String>()
+    S.chars()
+        .filter(
+            |c| {
+                *c >= '\u{03B1}' && *c <= '\u{03C9}' // lowercases
+                        || *c == '\u{03F2}'
+            }, // the lunar sigma
+        )
+        .collect::<String>()
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
-    
+
     #[test]
     fn test_core_text() {
-   
         let s = "16 Εἶπεν δὲ παραβολὴν πρὸς αὐτοὺς λέγων·
             ἀνθρώπου τινὸς πλουσίου εὐφόρησεν ἡ χώρα. 17 
             καὶ διελογίζετο ἐν ἑαυτῷ λέγων· τί ποιήσω, ὅτι 
@@ -102,4 +101,40 @@ mod tests {
 
         assert_eq!(core_text(String::from(s)), String::from(s2));
     }
+
+    /*
+       https://stackoverflow.com/questions/517923/what-is-the-best-way-to-remove-accents-normalize-in-a-python-unicode-string
+
+       https://crates.io/crates/hebrew_unicode_utils
+    
+       Tester fonctionnalite :
+        - (1) qu'il n'y ait plus d'accentuations
+        - (2) qu'il n'y ait plus de majuscules
+        - (3) qu'il n'y ait plus de sigma autre que le lunaire
+        - (4) qu'il n'y ait pas d'autre caractere que les 
+          caracteres alphabetique grec.
+       Tester une combinaison de deux :
+        - (1) et (2)
+        - (1) et (3)
+        - (1) et (4)
+        - (2) et (3)
+        - (2) et (4)
+        - (3) et (4)
+       Tester une combinaison de trois :
+        - (1) et (2) et (3)
+        - (1) et (2) et (4)
+        - (1) et (3) et (4)
+        - (2) et (3) et (4)
+      Tester toutes les fonctionnalite ensemble :
+        - (1) et (2) et (3) et (4)
+
+      Tester sur quelle donnee ?
+      (1) :
+          - (1.1) que sur des accents
+          - (1.2) que sur des caracteres accentues - un unicode
+          - (1.3) que sur des caractere accentues - deux unicodes
+          - (1.3) que sur des caracteres non accentues
+          - (1.4) que sur une chaine ayant des caracteres accentues et non accentues
+          
+    */
 }
